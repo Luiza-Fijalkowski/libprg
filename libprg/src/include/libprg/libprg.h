30be6a628fd7b3d67d9b2c3d7db70d808cdfc4b7
aluno <luiza.f2006@aluno.ifsc.edu.br>
2025-10-16 11:24:31 -0300

    lista encadeada dupla

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -16,28 +16,29 @@ void destruir_pilha(pilha_t *pilha);
 
     //----fila----
 
-// typedef struct fila fila_t;
-// fila_t* criar_fila(int capacidade);
-// void enfileirar(fila_t* f, int valor);
-// bool cheia(fila_t fila);
+ typedef struct fila fila_t;
+ fila_t* criar_fila(int capacidade);
+ void enfileirar(fila_t* fila, int valor);
+ bool cheia(fila_t fila);
+ void desenfileirar(fila_t* fila);
 
 
     //----lista-----
 
-// typedef struct lista lista_t;
-// bool vazia(lista_t* lista);
-// static bool cheia(lista_t* lista);
-// void inserir_nao_ordenada(lista_t *lista, int valor);
-// void inserir_ordenada(lista_t *lista, int valor);
-// int busca_linear(lista_t* lista, int valor);
-// int busca_binaria(lista_t* lista, int valor);
-// lista_t* criar_lista(int capacidade, bool ordenar);
-// void inserir_lista(lista_t* lista, int valor);
-// int buscar_lista(lista_t* lista, int valor);
-// void remover_item_lista(lista_t* lista, int valor);
-// void destruir_lista(lista_t* lista);
-// void listar_primeiro_elemento_e_tamanho(lista_t* lista);
-// void listar(lista_t* lista);
+ typedef struct lista lista_t;
+ bool vazia(lista_t* lista);
+ static bool cheia(lista_t* lista);
+ void inserir_nao_ordenada(lista_t *lista, int valor);
+ void inserir_ordenada(lista_t *lista, int valor);
+ int busca_linear(lista_t* lista, int valor);
+ int busca_binaria(lista_t* lista, int valor);
+ lista_t* criar_lista(int capacidade, bool ordenar);
+ void inserir_lista(lista_t* lista, int valor);
+ int buscar_lista(lista_t* lista, int valor);
+ void remover_item_lista(lista_t* lista, int valor);
+ void destruir_lista(lista_t* lista);
+ void listar_primeiro_elemento_e_tamanho(lista_t* lista);
+ void listar(lista_t* lista);
 
 
     //----lista_encadeada-----
@@ -52,5 +53,13 @@ void remover(no_t** inicio, int valor);
     //----lista_encadeada_circular----
 void adicionar_circular(no_t** inicio, int valor);
 void destruir_lista_encadeada_circular(no_t** inicio);
+
+    //----lista encadeada dupla----
+typedef struct no_duplo no_duplo_t;
+no_duplo_t** criar_lista_encadeada_dupla(int valor);
+void adicionar_lista_encadeada_dupla(no_duplo_t** inicio, int valor);
+no_duplo_t* buscar_lista_encadeada_dupla(no_duplo_t** inicio, int valor);
+void remover_lista_encadeada_dupla(no_duplo_t** inicio, int valor);
+void destruir_lista_encadeada_dupla(no_duplo_t** inicio);
 #endif
 
