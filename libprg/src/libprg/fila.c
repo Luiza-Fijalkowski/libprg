1e32d7580384726c1572eed4aec98c6fbd2a913a
aluno <luiza.f2006@aluno.ifsc.edu.br>
2026-03-26 17:28:47 -0300

    garantia

--- libprg/src/libprg/fila.c
+++ libprg/src/libprg/fila.c
@@ -26,6 +26,8 @@ fila_t* criar_fila(int capacidade) {
 
 void enfileirar(fila_t* fila, int valor) {
 
+    if(fila->fim >= fila->tamanho) exit(EXIT_FAILURE);
+
     fila->elementos[fila->fim] = valor;
     fila->fim = (fila->fim + 1) % fila->capacidade;
     fila->tamanho++;
