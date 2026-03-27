76bc69c74419efd79698568f095922f4f604a4e4
luiza <luiza.f2006@aluno.ifsc.edu.br>
2026-03-27 16:29:47 -0300

    açoes fila finalizadas

--- libprg/src/libprg/fila.c
+++ libprg/src/libprg/fila.c
@@ -32,12 +32,35 @@ void enfileirar(fila_t* fila, int valor) {
     fila->fim = (fila->fim + 1) % fila->capacidade;
     fila->tamanho++;
 }
-// desemfileirar
-// inicio
-// fim
-// vazia
-// cheia
+void desemfileirar(fila_t* fila, int valor) {
+    if (fila->inicio <= 0) exit(EXIT_FAILURE);
 
+    fila->elementos[fila->fim] = valor;
+    fila->fim = (fila->fim - 1) % fila->capacidade;
+    fila->tamanho--;
+}
+
+int inicio(fila_t* fila, int inicio) {
+    fila->inicio = inicio;
+    return fila->inicio;
+}
+
+int fim(fila_t* fila, int fim) {
+
+    fila->elementos[fim] = fila->elementos[fila->fim];
+    return fila->elementos[fila->fim];
+
+}
+
+int vazia(fila_t* fila, int capacidade) {
+
+    if(capacidade <= 0) exit(EXIT_FAILURE);
+}
+
+int cheia(fila_t* fila, int capacidade) {
+
+    if (capacidade >= 0) exit(EXIT_FAILURE);
+}
 
 int destruir_fila(fila_t* fila) {
 
