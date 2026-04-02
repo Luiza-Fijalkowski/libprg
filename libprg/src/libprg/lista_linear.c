35e3968b6c3fe4e9cdaa44106e5ae23837244079
aluno <luiza.f2006@aluno.ifsc.edu.br>
2026-04-02 17:29:40 -0300

    inicio da list5a

---
+++ libprg/src/libprg/lista_linear.c
@@ -0,0 +1,16 @@
+//
+// Created by aluno on 02/04/2026.
+//
+
+#include <stdbool.h>
+
+typedef struct lista_linear {
+    int* elementos;
+    int tamanho;
+    int cantidade;
+    bool ordenada;
+}lista_linear_t;
+
+int criar_lista_linear(int tamanho, int cantidade, bool ordenada) {
+
+}
